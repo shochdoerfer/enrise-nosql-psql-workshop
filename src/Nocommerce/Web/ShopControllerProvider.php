1de6e672d679f2ee0a89a5e6fc18fed5a5442e95
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2015-11-21 15:08:43 +0100

    Added code for a helper function to convert json strings into PHP objects.

--- src/Nocommerce/Web/ShopControllerProvider.php
+++ src/Nocommerce/Web/ShopControllerProvider.php
@@ -27,14 +27,26 @@ class ShopControllerProvider implements ControllerProviderInterface
         // creates a new controller based on the default route
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('/', function (Application $app) {
+        // helper function to json_decode rating and meta fields
+        $decodeJsonAttributes = function (array $products) {
+            // "converting" the json strings for rating and meta into native php datatypes
+            foreach ($products as $key => $product) {
+                $product['rating'] = json_decode($product['rating']);
+                $product['meta'] = json_decode($product['meta']);
+                $products[$key] = $product;
+            }
+
+            return $products;
+        };
+
+        $controllers->get('/', function (Application $app) use ($decodeJsonAttributes) {
             $sql = 'SELECT "id", "name", "description", "price", "categories" FROM "products"';
             $products = $app['db']->fetchAll($sql);
 
             return $app['twig']->render('index.twig', ['products' => $products]);
         });
 
-        $controllers->get('/browse/{category}', function (Application $app, $category) {
+        $controllers->get('/browse/{category}', function (Application $app, $category) use ($decodeJsonAttributes) {
             $sql = 'SELECT "id", "name", "description", "price", "categories" FROM "products" WHERE "categories" @> ARRAY[?]';
             $stmt = $app['db']->prepare($sql);
             $stmt->bindValue(1, $category, 'string');
@@ -44,7 +56,7 @@ class ShopControllerProvider implements ControllerProviderInterface
             return $app['twig']->render('index.twig', ['products' => $products]);
         });
 
-        $controllers->post('/search', function (Application $app, Request $request) {
+        $controllers->post('/search', function (Application $app, Request $request) use ($decodeJsonAttributes) {
             $query = $request->get('query');
             $sql = 'SELECT "id", "name", "description", "price", "categories" FROM "products" WHERE to_tsvector(\'english\',  "name" || \' \' || "description") @@ to_tsquery(\'english\', ?)';
             $stmt = $app['db']->prepare($sql);
@@ -56,7 +68,7 @@ class ShopControllerProvider implements ControllerProviderInterface
 
         });
 
-        $controllers->post('/rate', function (Application $app) {
+        $controllers->post('/rate', function (Application $app) use ($decodeJsonAttributes) {
             $rating = ['cnt' => 0, 'rating' => 0];
 
             return $app['twig']->render('ratings.twig', ['rating' => $rating]);
